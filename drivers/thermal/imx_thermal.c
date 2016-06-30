8899f536ff89d67ebf97887bbacad14b1a64f232
Your Name <Your Email>
2016-06-30 15:34:28 +0200

    thermal-temperature-change

--- drivers/thermal/imx_thermal.c
+++ drivers/thermal/imx_thermal.c
@@ -92,7 +92,7 @@ enum imx_thermal_trip {
  * It defines the temperature in millicelsius for passive trip point
  * that will trigger cooling action when crossed.
  */
-#define IMX_TEMP_PASSIVE		85000
+#define IMX_TEMP_PASSIVE		100000
 #define IMX_TEMP_PASSIVE_COOL_DELTA	10000
 
 #define IMX_POLLING_DELAY		2000 /* millisecond */
